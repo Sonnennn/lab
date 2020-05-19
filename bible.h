d59c23261de81730c2b664ab43e3807a6fca8d71
sonnen <youthilya@gmail.com>
2020-05-19 12:33:36 +0300

    окончательные правки

--- bible.h
+++ bible.h
@@ -1,12 +1,10 @@
-//
-// Created by Arkasha on 14.05.2020.
-//
 
-#ifndef UNTITLED1_BIBLE_H
-#define UNTITLED1_BIBLE_H
+
+#ifndef BIBLE_H
+#define BIBLE_H
 
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
 
-#endif //UNTITLED1_BIBLE_H
+#endif
